c65d88f15077f372654d70d7da19dc1d348a34c2
whjr1750 <91913630+whjr1750@users.noreply.github.com>
2024-02-14 10:38:28 +0530

    Update CreateVideo.py

--- CreateVideo.py
+++ CreateVideo.py
@@ -1,6 +1,16 @@
 import os
 import cv2
 
+import ssl
+
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+# Legacy Python that doesn't verify HTTPS certificates by default
+    pass
+else:
+# Handle target environment that doesn't support HTTPS verification
+    ssl._create_default_https_context = _create_unverified_https_context
 
 path = "Images"
 
